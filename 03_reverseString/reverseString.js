8e01160a80245b7c4a139faa124349c41b79bda4
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2022-11-14 20:51:09 +0100

    First 3 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let characterArray = string.split('');
+    let output = '';
+    
 
+    for (let i = characterArray.length - 1; i >= 0; i--) {
+        output += characterArray[i];
+    }
+
+    return output;
 };
 
 // Do not edit below this line
