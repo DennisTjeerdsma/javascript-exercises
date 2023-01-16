2b562d09df7ea03fba1bafba93749d1ba48b7568
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2023-01-16 19:26:14 +0100

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
+    var filteredArray = arguments[0];
 
+    for (var i = 1; i < arguments.length; i++) {
+        var filteredArray = filteredArray.filter(e => e !== arguments[i]);
+    }
+
+    return filteredArray
 };
 
 // Do not edit below this line
