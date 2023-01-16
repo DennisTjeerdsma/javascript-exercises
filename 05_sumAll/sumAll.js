f4d5b8bb27af0399dbf1f8bd14ef74b374638ed6
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2023-01-16 19:45:23 +0100

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let total = 0;
 
+    if ( a < 0 || b < 0  || !Number.isInteger(a) || !Number.isInteger(b) ) {
+        return "ERROR";
+    }
+
+    if (b < a) {
+        [a, b] = [b, a];
+    }
+
+    for (var i = a; i <= b; i++){
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
